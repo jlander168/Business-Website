efcd30a54772831687d74be942850a65ba0b6367
Jared <jlander168@learner.dvusd.org>
2020-03-13 15:19:21 -0700

    Creating a slideshow
    Created a slideshow that changes pictures onclick for the buttons
    stored the images in an array
    Added arrow buttons on each side that changes the picture to the next image in the array
    added dots on the bottom that changes the picture specifically
    added on hovers for the buttons and keyframes for the transitions

---
+++ index.js
@@ -0,0 +1,33 @@
+var images = ["images/children1.jpg", "images/children3.jpg", "images/Overwatch1.jpg"];
+var slide = document.getElementById("slideshow-images");
+var n = 0;
+var dots = document.getElementsByClassName("dot")
+
+function showImage() {
+    document.getElementById("slideshow").innerHTML = "<img src='"+images[n]+"' width='100%' id='slideshow-images' class='fade'>";
+    for(j=0;j<dots.length;j++){
+        dots[j].className = dots[j].className.replace(" active", "")
+    }
+    dots[n].className += " active"
+}
+function switchImage(i) {
+    if(i==-1){
+        n--;
+        if(n<0){
+            n=images.length-1;
+        }
+        return showImage();
+    }
+    if(i==1){
+        n++;
+        if(n>images.length-1){
+            n=0;
+        }
+        return showImage();
+    }
+}
+
+function switchImageWithDot(i) {
+    n=i;
+    showImage();
+}
