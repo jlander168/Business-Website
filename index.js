e16bd701d6626c7edfcaf0e8c49aa54dd421bb35
Jared <jalan902@learner.dvusd.org>
2020-03-26 12:25:57 -0700

    Forgot to do commits so everything here
    Finished creating the slideshow and adding buttons to switch images
    Added an about section to the main page to explain what the company is about
    Added option boxes to click on to go to different pages with the images in them
    Created a footer section with contact info and the location in it
    Added a new font to the footer
    Added an icon for all the pages 
    Added a page for each game and put in the prices for each day

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+// All switching images for the index page 
 var images = ["images/children1.jpg", "images/children3.jpg", "images/Overwatch1.jpg"];
 var slide = document.getElementById("slideshow-images");
 var n = 0;
 var dots = document.getElementsByClassName("dot")
 
 function showImage() {
-    document.getElementById("slideshow").innerHTML = "<img src='"+images[n]+"' width='100%' id='slideshow-images' class='fade'>";
+    document.getElementById("slideshow").innerHTML = "<img src='"+images[n]+"' width='100%' id='slideshow-images' class='fade'><div class='caption'>Best Summer Camp for Gamers</div>";
     for(j=0;j<dots.length;j++){
         dots[j].className = dots[j].className.replace(" active", "")
     }
